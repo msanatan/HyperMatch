02602bdfbf4ba598fc83faff4ce093c1016ca5e0
Marcus Sanatan <msanatan@gmail.com>
2020-10-05 22:07:35 -0400

    Initialize Phaser
    It's a mobile game, we'll let the height control the width

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('hello');
+import 'phaser';
+
+const config: Phaser.Types.Core.GameConfig = {
+  title: 'Hyper Match',
+  type: Phaser.AUTO,
+  parent: 'app',
+  width: 1080,
+  height: 1920,
+  scale: {
+    mode: Phaser.Scale.HEIGHT_CONTROLS_WIDTH,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
+  autoFocus: true,
+  render: {
+    pixelArt: true, // Prevents blurring, see docs https://photonstorm.github.io/phaser3-docs/Phaser.Core.Config.html#pixelArt
+  },
+  input: {
+    windowEvents: false
+  },
+  scene: {}
+};
+
+export default new Phaser.Game(config);
