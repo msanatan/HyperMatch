70218aa405a50c4fdc529420c64635ad2d1cd3c3
Marcus Sanatan <msanatan@gmail.com>
2020-10-06 22:51:56 -0400

    Record the texture of a tile
    We need to update the collector tile, so we keep this info

--- src/entities/TileSprite.ts
+++ src/entities/TileSprite.ts
@@ -1,8 +1,15 @@
 import 'phaser';
 
 export default class TileSprite extends Phaser.GameObjects.Sprite {
+  private _textureKey: string;
+
+  get textureKey(): string {
+    return this._textureKey;
+  }
+
   constructor(scene: Phaser.Scene, x: number, y: number, texture: string, frame: number = 0) {
     super(scene, x, y, texture, frame);
+    this._textureKey = texture;
     this.scene.add.existing(this);
     this.setOrigin(0, 0);
     this.setScale(1, 1.25);
