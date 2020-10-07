50d42a38adf785bcd50f7469709a84d59f1ec3d5
Marcus Sanatan <msanatan@gmail.com>
2020-10-06 22:52:34 -0400

    Add pointer events on tiles
    This updates the collector tile! Closer to a prototype

--- src/scenes/GameScene.ts
+++ src/scenes/GameScene.ts
@@ -1,6 +1,7 @@
 import 'phaser';
 import { DIFFICULTY } from '../constants';
 import TileSprite from '../entities/TileSprite';
+import CollectorSprite from '../entities/CollectorSprite';
 
 export default class GameScene extends Phaser.Scene {
   private difficulty: number;
@@ -19,6 +20,16 @@ export default class GameScene extends Phaser.Scene {
     this.tileGroup = this.add.group();
     this.collectorGroup = this.add.group();
     this.addTiles(this.difficulty);
+
+    this.input.setHitArea(this.tileGroup.getChildren()).
+      on('gameobjectdown', (pointer: PointerEvent, tileSprite: TileSprite) => {
+        // Find selected collected tile
+        this.collectorGroup.getChildren().forEach((collectorSprite: CollectorSprite) => {
+          if (collectorSprite.selected) {
+            collectorSprite.setTexture(tileSprite.textureKey);
+          }
+        });
+      });
   }
 
   addTiles(difficulty: number): void {
@@ -33,7 +44,7 @@ export default class GameScene extends Phaser.Scene {
         this.tileGroup.add(new TileSprite(this, 710, 1635, 'greyTile'));
 
         // Add collector tile
-        this.collectorGroup.add(new TileSprite(this, 390, 1150, 'blackTile'));
+        this.collectorGroup.add(new CollectorSprite(this, 390, 1150, true));
         break;
     }
   }
