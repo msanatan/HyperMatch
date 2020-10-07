be95fc6498a753fd6ffba535c9e8ee155783779d
Marcus Sanatan <msanatan@gmail.com>
2020-10-07 07:49:49 -0400

    Make tile an arcade sprite to have a velocity
    Admittedly we don't need other Physics things.
    We could have added velocity as properties but this is easier.

--- src/entities/TileSprite.ts
+++ src/entities/TileSprite.ts
@@ -1,6 +1,6 @@
 import 'phaser';
 
-export default class TileSprite extends Phaser.GameObjects.Sprite {
+export default class TileSprite extends Phaser.Physics.Arcade.Sprite {
   private _textureKey: string;
 
   get textureKey(): string {
@@ -10,8 +10,13 @@ export default class TileSprite extends Phaser.GameObjects.Sprite {
   constructor(scene: Phaser.Scene, x: number, y: number, texture: string, frame: number = 0) {
     super(scene, x, y, texture, frame);
     this._textureKey = texture;
-    this.scene.add.existing(this);
     this.setOrigin(0, 0);
     this.setScale(1, 1.25);
+    this.scene.physics.world.enable(this);
+    this.body = this.body as Phaser.Physics.Arcade.Body; // TypeScript cast
+    this.body.setGravity(0, 0);
+    this.body.setVelocity(0, 0);
+    this.body.setImmovable(true);
+    this.scene.add.existing(this);
   }
 }
