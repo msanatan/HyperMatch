8d2be60359477d765c8fa2f68ff11e2104c196c4
Marcus Sanatan <msanatan@gmail.com>
2020-10-06 21:52:06 -0400

    Preload all the assets at startup

---
+++ src/scenes/PreloadScene.ts
@@ -0,0 +1,19 @@
+import 'phaser';
+
+export default class PreloadScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'PreloadScene' });
+  }
+
+  preload(): void {
+    this.load.pack(
+      'hyperMatchPack',
+      'pack.json',
+      'hyperMatchPack'
+    );
+  }
+
+  update(time: number, delta: number): void {
+    this.scene.start('GameScene');
+  }
+}
