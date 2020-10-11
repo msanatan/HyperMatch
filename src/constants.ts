a43a2bc19d270895edb2b4bd8e2969d9a399c6d4
Marcus Sanatan <msanatan@gmail.com>
2020-10-10 23:57:39 -0400

    Inclue reference to ball texture in colour map

--- src/constants.ts
+++ src/constants.ts
@@ -7,18 +7,19 @@ export enum DIFFICULTY {
 export interface TextureColourMap {
   name: string;
   tile?: string;
+  ball?: string;
   collector: string;
 }
 
 export const tileTextureColours: TextureColourMap[] = [
-  { name: 'red', tile: 'redTile', collector: 'collectorRedTile' },
-  { name: 'blue', tile: 'blueTile', collector: 'collectorBlueTile' },
-  { name: 'yellow', tile: 'yellowTile', collector: 'collectorYellowTile' },
-  { name: 'green', tile: 'greenTile', collector: 'collectorGreenTile' },
-  { name: 'purple', tile: 'purpleTile', collector: 'collectorPurpleTile' },
-  { name: 'grey', tile: 'greyTile', collector: 'collectorGreyTile' },
-  { name: 'orange', tile: 'orangeTile', collector: 'collectorOrangeTile' },
-  { name: 'pink', tile: 'pinkTile', collector: 'collectorPinkTile' },
-  { name: 'brown', tile: 'brownTile', collector: 'collectorBrownTile' },
+  { name: 'red', tile: 'redTile', collector: 'collectorRedTile', ball: 'redBall' },
+  { name: 'blue', tile: 'blueTile', collector: 'collectorBlueTile', ball: 'blueBall' },
+  { name: 'yellow', tile: 'yellowTile', collector: 'collectorYellowTile', ball: 'yellowBall' },
+  { name: 'green', tile: 'greenTile', collector: 'collectorGreenTile', ball: 'greenBall' },
+  { name: 'purple', tile: 'purpleTile', collector: 'collectorPurpleTile', ball: 'purpleBall' },
+  { name: 'grey', tile: 'greyTile', collector: 'collectorGreyTile', ball: 'greyBall' },
+  { name: 'orange', tile: 'orangeTile', collector: 'collectorOrangeTile', ball: 'orangeBall' },
+  { name: 'pink', tile: 'pinkTile', collector: 'collectorPinkTile', ball: 'pinkBall' },
+  { name: 'brown', tile: 'brownTile', collector: 'collectorBrownTile', ball: 'brownBall' },
   { name: 'black', collector: 'collectorBlackTile' }
 ];
