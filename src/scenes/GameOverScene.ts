9082c04eb4bde2ed878adf2cd5b33cb9df9d44b5
Marcus Sanatan <msanatan@gmail.com>
2020-10-11 22:49:44 -0400

    Add game over scene
    Simple. Shows score + buttons to retry or back to title screen

---
+++ src/scenes/GameOverScene.ts
@@ -0,0 +1,83 @@
+import 'phaser';
+
+enum STATE {
+  EXIT,
+  RETRY
+}
+
+export default class GameOverScene extends Phaser.Scene {
+  constructor() {
+    super({ key: 'GameOverScene' });
+  }
+
+  create(): void {
+    this.cameras.main.fadeIn(500, 0, 0, 0);
+    let state: STATE;
+    const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
+    const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
+
+    // Game over text
+    const gameOverTextConfig = {
+      fontFamily: 'Gugi Regular, Helvetica, Arial',
+      fontSize: '144px',
+      color: '#FFFFFF',
+    };
+    const gameOverText = this.add.text(screenCenterX, screenCenterY - 500, 'GAME OVER', gameOverTextConfig);
+    gameOverText.setOrigin(0.5);
+
+    // Score text
+    const scoreTextConfig = {
+      fontFamily: 'Gugi Regular, Helvetica, Arial',
+      fontSize: '108px',
+      color: '#FFFFFF',
+    };
+    const scoreText = this.add.text(screenCenterX, screenCenterY - 300, `Score: ${this.registry.get('score')}`, scoreTextConfig);
+    scoreText.setOrigin(0.5);
+
+    const btnTextConfig = {
+      fontFamily: 'Gugi Regular, Helvetica, Arial',
+      fontSize: '96px',
+      color: '#FFFFFF',
+    };
+
+    // Exit Button
+    const btnExit = this.add.sprite(screenCenterX - 200, screenCenterY + 100, 'btnExit');
+    btnExit.setOrigin(0.5);
+    btnExit.setScale(0.4, 0.4);
+    const exitText = this.add.text(screenCenterX - 25, screenCenterY + 100, 'Exit', btnTextConfig);
+    exitText.setOrigin(0.5);
+
+    // Retry button
+    const btnRetry = this.add.sprite(screenCenterX - 200, screenCenterY + 275, 'btnRetry');
+    btnRetry.setOrigin(0.5);
+    btnRetry.setScale(0.4, 0.4);
+    const retryText = this.add.text(screenCenterX + 15, screenCenterY + 275, 'Retry', btnTextConfig);
+    retryText.setOrigin(0.5);
+
+    // Button actions
+    btnExit.setInteractive();
+    btnExit.on('pointerdown', () => {
+      this.cameras.main.fadeOut(250, 0, 0, 0);
+      state = STATE.EXIT;
+    });
+
+    btnRetry.setInteractive();
+    btnRetry.on('pointerdown', () => {
+      this.cameras.main.fadeOut(250, 0, 0, 0);
+      state = STATE.RETRY;
+    });
+
+    this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, () => {
+      switch (state) {
+        case STATE.EXIT:
+          this.scene.start('TitleScene');
+          break;
+        case STATE.RETRY:
+          this.scene.start('GameScene');
+          this.scene.start('HUDScene');
+          this.scene.bringToTop('HUDScene');
+          break;
+      }
+    });
+  }
+}
