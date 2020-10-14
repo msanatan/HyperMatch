c4da2f9ea51ac4ee7aa834f71d7dd68d275d692d
Marcus Sanatan <msanatan@gmail.com>
2020-10-13 21:58:01 -0400

    Add Paused scene
    This menu allows players to resume their game or exit.
    It also contians a semi-transparent overlay, a nice touch

---
+++ src/scenes/PausedScene.ts
@@ -0,0 +1,71 @@
+import 'phaser';
+
+export default class PausedScene extends Phaser.Scene {
+
+  constructor() {
+    super({ key: 'PausedScene' });
+  }
+
+  create() {
+    // Semi-transparent overlay
+    const graphics = this.add.graphics();
+    graphics.fillRect(
+      0,
+      0,
+      <number>this.game.config.width,
+      <number>this.game.config.height
+    );
+    graphics.fillStyle(0x000000, 0.3);
+
+    // Paused text
+    const pauseTextConfig = {
+      fontFamily: 'Gugi Regular, Helvetica, Arial',
+      fontSize: '144px',
+      color: '#FFFFFF',
+    };
+
+    const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
+    const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
+    const pauseText = this.add.text(screenCenterX, screenCenterY - 600, 'PAUSED', pauseTextConfig);
+    pauseText.setOrigin(0.5);
+
+    // Paused buttons
+    const btnTextConfig = {
+      fontFamily: 'Gugi Regular, Helvetica, Arial',
+      fontSize: '96px',
+      color: '#FFFFFF',
+    };
+
+    // Resume Button
+    const btnResume = this.add.sprite(screenCenterX - 200, screenCenterY - 300, 'btnResume');
+    btnResume.setOrigin(0.5);
+    btnResume.setScale(0.4, 0.4);
+    const retryText = this.add.text(screenCenterX + 80, screenCenterY - 300, 'Resume', btnTextConfig);
+    retryText.setOrigin(0.5);
+
+    // Exit button
+    const btnExit = this.add.sprite(screenCenterX - 200, screenCenterY - 125, 'btnExit');
+    btnExit.setOrigin(0.5);
+    btnExit.setScale(0.4, 0.4);
+    const exitText = this.add.text(screenCenterX - 25, screenCenterY - 125, 'Exit', btnTextConfig);
+    exitText.setOrigin(0.5);
+
+    // Button input handlers
+    btnResume.setInteractive();
+    btnResume.on('pointerdown', () => {
+      this.scene.sleep();
+      this.scene.resume('GameScene');
+    });
+
+    btnExit.setInteractive();
+    btnExit.on('pointerdown', () => {
+      this.cameras.main.fadeOut(250, 0, 0, 0);
+    });
+
+    this.cameras.main.once(Phaser.Cameras.Scene2D.Events.FADE_OUT_COMPLETE, () => {
+      this.scene.stop('GameScene');
+      this.scene.stop('HUDScene');
+      this.scene.start('TitleScene');
+    });
+  }
+}
