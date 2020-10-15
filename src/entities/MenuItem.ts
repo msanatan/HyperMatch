66f4a0b30410aedfb4ec88419039b43637833495
Marcus Sanatan <msanatan@gmail.com>
2020-10-15 07:44:19 -0400

    Create class for menu items
    Refactors some common setup. We also want to have a notion of selected

---
+++ src/entities/MenuItem.ts
@@ -0,0 +1,13 @@
+import 'phaser';
+
+export default class MenuItem extends Phaser.GameObjects.Text {
+  private selected: boolean;
+
+  constructor(scene: Phaser.Scene, x: number, y: number, text: string, style: Phaser.Types.GameObjects.Text.TextStyle, selected: boolean = false) {
+    super(scene, x, y, text, style);
+    this.selected = selected;
+    this.setOrigin(0.5);
+    this.setInteractive();
+    this.scene.add.existing(this);
+  }
+}
