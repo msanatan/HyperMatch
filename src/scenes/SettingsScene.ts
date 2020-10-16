4de075707c21c886c5f14f71d0d0d0b6dff727fd
Marcus Sanatan <msanatan@gmail.com>
2020-10-15 23:12:30 -0400

    Add settings scene
    We'll have to add something to highlight the currently selected option.
    And a real transition to and from the title menu

---
+++ src/scenes/SettingsScene.ts
@@ -0,0 +1,81 @@
+import 'phaser';
+import { titleTextConfig, subtitleTextConfig, btnTextConfig, DIFFICULTY } from '../constants';
+import MenuItem from '../entities/MenuItem';
+
+export default class SettingsScene extends Phaser.Scene {
+  private selectDifficulty: Function;
+
+  constructor() {
+    super({ key: 'SettingsScene' });
+  }
+
+  create(): void {
+    const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
+    const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
+
+    // Title text
+    const settingsText = this.add.text(screenCenterX, screenCenterY - 600, 'SETTINGS', titleTextConfig);
+    settingsText.setOrigin(0.5);
+
+    // Subtitle text
+    const scoreText = this.add.text(screenCenterX, screenCenterY - 350, 'DIFFICULTY', subtitleTextConfig);
+    scoreText.setOrigin(0.5);
+
+    // Buttons
+    const btnEasy = new MenuItem(this, screenCenterX, scoreText.y + 150, 'Easy', btnTextConfig);
+    const btnMedium = new MenuItem(this, screenCenterX, btnEasy.y + 120, 'Medium', btnTextConfig);
+    const btnHard = new MenuItem(this, screenCenterX, btnMedium.y + 120, 'Hard', btnTextConfig);
+
+    // Button pointer listener
+    const difficultyButtons = this.add.group([btnEasy, btnMedium, btnHard]);
+    this.selectDifficulty = (pointer: PointerEvent, difficultyBtn: MenuItem) => {
+      // Find selected collected tile
+      difficultyButtons.getChildren().forEach((btn: MenuItem) => {
+        if (btn.selected) {
+          btn.selected = false;
+        }
+      });
+
+      difficultyBtn.selected = true;
+      switch (difficultyBtn.text) {
+        case 'Easy':
+          this.registry.set('difficulty', DIFFICULTY.EASY);
+          break;
+        case 'Medium':
+          this.registry.set('difficulty', DIFFICULTY.MEDIUM);
+          break;
+        case 'Hard':
+          this.registry.set('difficulty', DIFFICULTY.HARD);
+          break;
+        default:
+          this.registry.set('difficulty', DIFFICULTY.EASY);
+      }
+    };
+
+    this.input.setHitArea(difficultyButtons.getChildren()).
+      on('gameobjectdown', this.selectDifficulty);
+
+    // Make button text larger when hovering
+    this.input.setHitArea(difficultyButtons.getChildren()).
+      on('gameobjectover', (pointer: PointerEvent, difficultyBtn: MenuItem) => {
+        difficultyBtn.setFontSize(120);
+      });
+
+    this.input.setHitArea(difficultyButtons.getChildren()).
+      on('gameobjectout', (pointer: PointerEvent, difficultyBtn: MenuItem) => {
+        difficultyBtn.setFontSize(96);
+      });
+
+    // Back button
+    const btnBack = this.add.sprite(screenCenterX - 200, btnHard.y + 550, 'btnBack');
+    btnBack.setOrigin(0.5);
+    btnBack.setScale(0.4, 0.4);
+    const backText = new MenuItem(this, btnBack.x + 200, btnHard.y + 550, 'Back', btnTextConfig);
+
+    // Button input handlers
+    btnBack.setInteractive();
+    btnBack.on('pointerdown', () => {
+      this.scene.start('TitleScene');
+    });
+  }
+}
