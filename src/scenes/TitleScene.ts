a34a9e44be4518a8fe649854f6a271a4bd705866
Marcus Sanatan <msanatan@gmail.com>
2020-10-18 19:26:36 -0400

    Title looks better

--- src/scenes/TitleScene.ts
+++ src/scenes/TitleScene.ts
@@ -1,5 +1,5 @@
 import 'phaser';
-import { DIFFICULTY, titleTextConfig, btnTextConfig, changeFontSize } from '../constants';
+import { DIFFICULTY, titleTextConfig, changeFontSize, subtitleTextConfig } from '../constants';
 import MenuItem from '../entities/MenuItem';
 
 export default class TitleScene extends Phaser.Scene {
@@ -18,17 +18,17 @@ export default class TitleScene extends Phaser.Scene {
 
     const screenCenterX = this.cameras.main.worldView.x + this.cameras.main.width / 2;
     const screenCenterY = this.cameras.main.worldView.y + this.cameras.main.height / 2;
-    const titleText = this.add.text(screenCenterX, 256, 'Hyper Match', titleTextConfig);
+    const titleText = this.add.text(screenCenterX, screenCenterY - 600, 'HYPER MATCH', titleTextConfig);
     titleText.setOrigin(0.5);
 
     // Play button
-    const btnPlay = new MenuItem(this, screenCenterX, screenCenterY, 'Play', btnTextConfig);
-    const btnSettings = new MenuItem(this, btnPlay.x, btnPlay.y + 150, 'Settings', btnTextConfig);
+    const btnPlay = new MenuItem(this, screenCenterX, screenCenterY, 'Play', subtitleTextConfig);
+    const btnSettings = new MenuItem(this, btnPlay.x, btnPlay.y + 150, 'Settings', subtitleTextConfig);
     const btnGroup = this.add.group([btnPlay, btnSettings]);
 
     // Make button text larger when hovering
     this.increaseFont = changeFontSize(120);
-    this.decreaseFont = changeFontSize(96);
+    this.decreaseFont = changeFontSize(104);
     this.input.setHitArea(btnGroup.getChildren()).
       on('gameobjectover', this.increaseFont).
       on('gameobjectout', this.decreaseFont);
