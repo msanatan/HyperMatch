07c82e23322fa25177d5c2b1461c56017efc157d
Marcus Sanatan <msanatan@gmail.com>
2020-10-18 00:51:14 -0400

    Ensure that all tiles are interactive when created

--- src/entities/TileSprite.ts
+++ src/entities/TileSprite.ts
@@ -12,11 +12,13 @@ export default class TileSprite extends Phaser.Physics.Arcade.Sprite {
     this._colour = colour;
   }
 
-  constructor(scene: Phaser.Scene, x: number, y: number, texture: string, colour: TextureColourMap, frame: number = 0, ) {
+  constructor(scene: Phaser.Scene, x: number, y: number, texture: string,
+    colour: TextureColourMap, frame: number = 0) {
     super(scene, x, y, texture, frame);
     this._colour = colour;
     this.setOrigin(0, 0);
     this.setScale(0.8, 0.8);
+    this.setInteractive();
     this.scene.physics.world.enable(this);
     (<Phaser.Physics.Arcade.Body>this.body).
       setGravity(0, 0).
